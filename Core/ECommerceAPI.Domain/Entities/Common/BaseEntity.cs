f0fb79edfb2263ec364f36dab02deb7e1a55ce07
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-05-04 23:46:59 +0300

    Add project files.

---
+++ Core/ECommerceAPI.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Domain.Entities.Common
+{
+    public class BaseEntity
+    {
+
+        public Guid Id { get; set; } // Guid = Uniqe Identity Field
+        public DateTime CreatedDate { get; set; }
+
+
+    }
+}
