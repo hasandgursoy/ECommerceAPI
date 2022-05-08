506ea8f325957166450e2bd057dc5b786c9730e8
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-05-08 23:47:23 +0300

    Tracking

--- Core/ECommerceAPI.Application/Repositories/IReadRepository.cs
+++ Core/ECommerceAPI.Application/Repositories/IReadRepository.cs
@@ -10,11 +10,18 @@ namespace ECommerceAPI.Application.Repositories
 {
     public interface IReadRepository<T> : IRepository<T> where T : BaseEntity
     {
-        IQueryable<T> GetAll();
-        IQueryable<T> GetWhere(Expression<Func<T,bool>>method);
-        Task<T> GetSingleAsync(Expression<Func<T,bool>> method);
-        Task<T> GetByIdAsync(string id);
+        IQueryable<T> GetAll(bool tracking = true);
+        IQueryable<T> GetWhere(Expression<Func<T,bool>>method,bool tracking = true);
+        Task<T> GetSingleAsync(Expression<Func<T,bool>> method, bool tracking = true);
+        Task<T> GetByIdAsync(string id, bool tracking = true);
 
 
     }
 }
+
+// Tracking
+// Tracking mekanizması EF core'da Database'deki değişiklikleri kontrol eden ve bildiren yapdıdır.
+// Eğer biz bunu her sorgu için her endpoint için kontrol etsin modunda bırakırsak bu maliyetli bir yapı haline döner.
+// O yüzden Read sınıfında amacımız değişiklik yapmak olmadığı için bunu kapatmak daha makul.
+// ReadRepository'de bunu yapma şeklimiz var ilk önce Table'ı bir Querayble bir yapıya çeviriyoruz.
+// Daha sonra eğer tracking false ise bunu Table.AsNoTracking() şeklinde güncelliyoruz ve istenilen yapıyı dönüyoruz.
