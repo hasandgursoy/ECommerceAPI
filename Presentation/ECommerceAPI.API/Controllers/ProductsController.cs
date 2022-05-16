32ddf111de71fa98d3377259fbd59703ef120c7b
Hasan Doğacan Gürsoy <hasan.d.gursoy@gmail.com>
2022-05-16 10:05:53 +0300

    Entry to API Calss for EndPoint

--- Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
+++ Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
@@ -1,8 +1,10 @@
 ﻿using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Application.ViewModels.Products;
 using ECommerceAPI.Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace ECommerceAPI.API.Controllers
 {
@@ -23,11 +25,50 @@ namespace ECommerceAPI.API.Controllers
         // Task yazmayı unutursak DbContext dispose olur.
         public async Task<IActionResult> Get()
         {
-             var result = await _productReadRepository.GetAll().ToListAsync();
-             return Ok(result);
+            return Ok(_productReadRepository.GetAll(false));
         }
 
-        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            return Ok(_productReadRepository.GetByIdAsync(id,false)); 
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Post(VM_Create_Product model)
+        {
+            await _productWriteRepository.AddAsync(new()
+            {
+                Name = model.Name,
+                Price = model.Price,
+                Stock = model.Stock,
+            });
+            await _productWriteRepository.SaveAsync();
+            return StatusCode((int)HttpStatusCode.Created);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(VM_Update_Product model)
+        {
+
+            Product product = await _productReadRepository.GetByIdAsync(model.Id);
+            product.Stock = model.Stock;
+            product.Price = model.Price;
+            product.Name = model.Name;
+            await _productWriteRepository.SaveAsync();
+
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _productWriteRepository.RemoveAsync(id);
+            await _productWriteRepository.SaveAsync();
+            return Ok();
+        }
 
     }
 }
